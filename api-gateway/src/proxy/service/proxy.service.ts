7fc022696a7f7dad7c00a546a2aa2e77a53366f9
Gabriel Silva <gabriel-blz@outlook.com.br>
2026-09-16 21:27:44 -0300

    🚧 add proxyService

--- api-gateway/src/proxy/service/proxy.service.ts
+++ api-gateway/src/proxy/service/proxy.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
-export class ProxyService {}
+export class ProxyService {
+    private readonly logger = new Logger(ProxyService.name);
+    
+    constructor(private readonly httpService: HttpService) {
+
+    }
+
+}
